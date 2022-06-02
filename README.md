31eb70ba271964c7aab4392f92087e6c7cac1092
dennis <wakahiad@gmail.com>
2022-06-02 11:41:20 +0300

    final commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Navbar Demo
+
+Responsive navbar built in React js
+
+Click [here](https://navbar-demo-ke.netlify.app) to visit demo
+
+DEnnis Githinji 🚀️
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,62 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
+import React, {useState, useRef, useEffect} from 'react'
+import {FaBars, FaFacebook, FaLinkedin, FaTwitter} from 'react-icons/fa'
+import {links, social} from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+    const [showLinks, setShowLinks] = useState(false);
+
+    const linksContainerRef = useRef(null);
+    const linksRef = useRef(null)
+
+    useEffect(() => {
+        const linksHeight = linksRef.current.getBoundingClientRect().height;
+        if (showLinks) {
+            linksContainerRef.current.style.height = `${linksHeight}px`
+        } else {
+            linksContainerRef.current.style.height = '0'
+        }
+    }, [showLinks])
+
+    return (
+        <nav>
+            <div className="nav-center">
+                <div className="nav-header">
+                    <img src={logo} alt="logo"/>
+                    <button className='nav-toggle' onClick={() => setShowLinks(!showLinks)}>
+                        <FaBars/>
+                    </button>
+                </div>
+                <div className={`${showLinks ? 'links-container show-container' : 'links-container'}`}
+                     ref={linksContainerRef}>
+                    <ul className='links' ref={linksRef}>
+                        {links.map((link) => {
+                            const {id, url, text} = link;
+                            return (
+                                <li key={id}>
+                                    <a href={url}>
+                                        {text}
+                                    </a>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+                <ul className="social-icons">
+                    <li>
+                        {social.map((socialIcon) => {
+                            const {id, url, icon} = socialIcon;
+                            return (
+                                <a key={id} href={url}>
+                                    {icon}
+                                </a>
+                            )
+                        })}
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
